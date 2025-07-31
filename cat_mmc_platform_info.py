310631916cfa922e50b9964b6e0a50ae47105a4e
adamHSU <Adam.Hsu@.quantatw.com>
2025-07-31 16:32:20 +0800

    feat: add function to send commands via SSH to TTY port

--- cat_mmc_platform_info.py
+++ cat_mmc_platform_info.py
@@ -5,6 +5,7 @@ import time
 import lib.ssh_util as ssh_util
 import lib.bmc_boot_utils as bbu
 from lib.init_logger import init_logger
+import lib.bmc_util as bu
 
 def run_test_cycle(ip, cycle_num, logger):
     logger.info(f"Running test cycle {cycle_num}...")
@@ -17,38 +18,11 @@ def run_test_cycle(ip, cycle_num, logger):
     stdin, stdout, stderr = ssh.exec_command("lsusb -vt")
     logger.info(stdout.read().decode())
 
-    """
-    if you want to write ten times per lopp as below command:
-
-    LOG=/tmp/uart.log; PORT=/dev/ttyUSB1;
-    stty -F "$PORT" 57600 cs8 -cstopb -parenb -icanon -echo;
-    > "$LOG";
-    cat "$PORT" >> "$LOG" & CATPID=$!;
-    sleep 2;
-    for i in {0..10};
-    do printf "platform info\r" > "$PORT"; sleep 0.1; done; sleep 2;
-    kill "$CATPID";
-    wait "$CATPID" 2>/dev/null;
-    cat "$LOG"
-
-    """
     # Display mmc platform info
     logger.info("Reading mmc platform info...")
-    command = '''
-    LOG=/tmp/uart.log; PORT=/dev/ttyUSB5; 
-    stty -F "$PORT" 57600 cs8 -cstopb -parenb -icanon -echo; 
-    > "$LOG"; 
-    cat "$PORT" >> "$LOG" & CATPID=$!; 
-    sleep 2; 
-    printf "platform info\r" > "$PORT"; 
-    sleep 2; 
-    kill "$CATPID"; 
-    wait "$CATPID" 2>/dev/null; 
-    cat "$LOG"
-    '''
-    stdin, stdout, stderr = ssh.exec_command(command)
-    logger.info(stdout.read().decode())
-
+    mmc = bu.SerialSSHClient(ssh, "ttyUSB5", logger)
+    logger.info(mmc.send_command("platform info"))
+    mmc.close()
     ssh.close()
     return True  # Cycle passed
 
@@ -56,6 +30,7 @@ def main(ip):
     # Initialize logger
     logger = init_logger("bmc_reboot.log", verbose=True)
     total_cycles = 1
+    failure_count = 0
 
     # Run 1 test cycles
     for cycle_num in range(1, total_cycles+1):
@@ -72,6 +47,6 @@ def main(ip):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-ip", help="Target BMC IP address", default="10.10.14.229")  # Default IP set here
+    parser.add_argument("-ip", help="Target BMC IP address", default="10.10.15.221")  # Default IP set here
     args = parser.parse_args()
     main(args.ip)
---
+++ lib/bmc_util.py
@@ -0,0 +1,56 @@
+import lib.ssh_util as ssh_util
+import time
+from logging import Logger
+from paramiko import SSHClient
+import re
+
+class SerialSSHClient:
+    def __init__(self, ssh: SSHClient, port: str, logger: Logger, log_file= "/tmp/uart.log", rate = 57600):
+        self.ssh = ssh
+        self.port = "/dev/" + port
+        self.log_file = log_file
+        self.logger = logger
+        self.pid = None
+        self.rate = str(rate)
+        self.setup_logging_process()
+
+    def setup_logging_process(self):
+        if self.pid:
+            self.close()
+        self.ssh.exec_command(f"stty -F {self.port} {self.rate} cs8 -cstopb -parenb -icanon -echo")
+        self.ssh.exec_command(f"> {self.log_file}")
+        stdin, stdout, stderr = self.ssh.exec_command(f"cat {self.port} >> {self.log_file} & echo $!")
+        self.pid = stdout.readline()
+        self.logger.info(f"Setup remote background process PID: {self.pid}")
+        return 
+    
+    def send_command(self, command: str, clear_log_file= True, sleep= 0.1):
+        if clear_log_file:
+            self.ssh.exec_command(f"> {self.log_file}")
+            time.sleep(sleep)
+        self.ssh.exec_command(f'printf "{command}\\r" > "{self.port}"')
+        time.sleep(sleep)
+        stdin, stdout, stderr = self.ssh.exec_command(f"cat {self.log_file}")
+        return self.__remove_ansi_escape(stdout.read().decode())
+    
+    def return_log(self):
+        stdin, stdout, stderr = self.ssh.exec_command(f"cat {self.log_file}")
+        return self.__remove_ansi_escape(stdout.read().decode())
+    
+    def clear_log(self):
+        self.ssh.exec_command(f"> {self.log_file}")
+        return
+    
+    def close(self):
+        self.ssh.exec_command(f"rm {self.log_file}")
+        command = f"""
+        kill {self.pid};
+        wait {self.pid} 2>/dev/null;
+        """
+        self.ssh.exec_command(command)
+        self.logger.info(f"Remote background process PID closed")
+        return
+    
+    def __remove_ansi_escape(self, text: str) -> str:
+        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
+        return ansi_escape.sub('', text)
